1338797449e1dc4a3f13693058a88df2d8dafe17
markjohn519 <markjohnmonton@gmail.com>
2022-09-27 09:43:35 +0800

    test1

---
+++ rest.js
@@ -0,0 +1,10 @@
+function sumUp(...toAdd){
+  console.log(toAdd)
+  let result = 0
+  for (let i = 0; i < toAdd.length; i++){
+    result += toAdd[i]
+  }
+  return result
+}
+
+console.log(sumUp(100,200, "000", 1))
